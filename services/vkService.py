8e12d3cde25e6f169d5601af25a0ec66e5601662
Exide-PC <ig.roman95@mail.ru>
2020-05-24 23:02:12 +0300

    Logging music id.

--- services/vkService.py
+++ services/vkService.py
@@ -36,7 +36,7 @@ class VkService:
         async def item_callback():
             id = audio['id']
             cached_path = self._vkCacheRepository.try_get_v2(id)
-            logging.info(f'Music id cache was found: ' + str(bool(cached_path)))
+            logging.info(f'Music cache for id {id} was found: ' + str(bool(cached_path)))
 
             if (cached_path):
                 return cached_path
